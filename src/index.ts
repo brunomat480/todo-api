0063d582139d397bfacf853c6b3c640e40619601
Bruno José <bru.matias100@gmail.com>
2025-11-20 18:06:19 -0300

    feat: creating endpoint chat

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ fastify.get('/tasks', async function (_request, reply: FastifyReply) {
 
 interface TaskBody {
   title: string;
+  checked?: boolean;
 }
 
 fastify.post('/tasks', async function (request: FastifyRequest<{Body: TaskBody}>, reply: FastifyReply) {
@@ -27,13 +28,12 @@ fastify.post('/tasks', async function (request: FastifyRequest<{Body: TaskBody}>
 
     const task = await prisma.task.create({
       data: {
-        title: title.trim()
+        title: title,
       }
     });
 
     return reply.status(201).send(task);
   } catch (error) {
-    console.error('Erro ao criar tarefa:', error);
 
     return reply.status(500).send({
       error: 'Erro interno do servidor'
@@ -79,7 +79,6 @@ fastify.patch('/tasks/:id/toggle', async function(request: FastifyRequest<{Param
 
     return reply.send(task);
   } catch (error) {
-    console.error('Erro ao criar tarefa:', error);
 
     return reply.status(500).send({
       error: 'Erro interno do servidor'
@@ -99,8 +98,147 @@ fastify.delete('/tasks/:id', async function(request: FastifyRequest<{Params: Tas
 
     return reply.status(204).send();
   } catch (error) {
-    console.error('Erro ao criar tarefa:', error);
 
+    return reply.status(500).send({
+      error: 'Erro interno do servidor'
+    });
+  }
+});
+
+interface ChatBody {
+  message: string;
+}
+
+interface Content {
+  action: 'create' | 'delete' | 'complete';
+  id: string;
+  title: string;
+  checked: boolean;
+  response: string;
+}
+
+interface Message {
+  role: 'assistant' | 'user' | 'system';
+  content: Content | string;
+}
+
+interface Choice {
+  index: number;
+  message: Message;
+  logprobs: null | unknown;
+  finish_reason: string;
+}
+
+interface OpenAIResponse {
+  choices: Choice[];
+}
+
+fastify.post('/ai/chat', async function (request: FastifyRequest<{Body: ChatBody}>, reply: FastifyReply) {
+  try {
+    const {message} = request.body;
+
+    if (!message) {
+      return reply.code(400).send({ error: 'message required' });
+    }
+
+    const currentTasks = await prisma.task.findMany()
+
+    const prompt = `Você é um assistente que ajuda a gerenciar uma lista de tarefas.
+
+    Tarefas atuais: ${JSON.stringify(currentTasks)}
+
+    Interprete a solicitação do usuário e responda APENAS com um JSON no formato:
+    {
+      "action": "create" | "delete" | "complete" | "list",
+      "id": string,
+      "title": string,
+      "response": string,
+      "checked": boolean
+    }
+
+    Regras para o campo "checked":
+    - Deve ser true quando a ação for "complete".
+    - Deve ser false para ações "create", "delete", "list" e "chat".
+
+    Reconheça sinônimos:
+    - criar / adicionar / incluir / nova tarefa
+    - excluir / remover / apagar / deletar
+    - concluir / finalizar / completar / marcar como feita
+    - listar / mostrar / ver tarefas
+
+    Para identificar qual tarefa, procure por nome similar ou posição.
+
+    Não é permitido executar ações envolvendo múltiplas tarefas na mesma requisição. Cada operação deve ser realizada individualmente.
+    Se o usuário mencionar mais de uma tarefa em uma única mensagem, o assistente deve retornar um aviso informando que apenas uma tarefa pode ser manipulada por vez.`;
+
+    const response = await fetch(`${process.env.API_GROQ_BASE_URL}/chat/completions`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${process.env.API_KEY_GROQ}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        model: process.env.MODEL_GROQ,
+        messages: [
+          {
+            role: 'system',
+            content: prompt
+          },
+          {
+            role: 'user',
+            content: message
+          }
+        ]
+      }),
+    });
+    const data: OpenAIResponse = await response.json();
+
+    const raw = data.choices[0]?.message.content || '';
+
+    const jsonMatch = String(raw).match(/\{[\s\S]*\}/);
+
+    if (!jsonMatch) {
+      throw new Error('Nenhum JSON válido encontrado na resposta da IA');
+    }
+
+    const choices: Content = JSON.parse(jsonMatch[0]);
+
+    switch (choices.action) {
+      case 'create': {
+        await prisma.task.create({
+          data: {
+            title: choices.title,
+            checked: choices.checked
+          }
+        });
+
+        break;
+      }
+      case 'complete': {
+        await prisma.task.update({
+          where: {
+            id: choices.id
+          },
+          data: {
+            checked: choices.checked
+          }
+        });
+
+        break;
+      }
+      case 'delete': {
+        await prisma.task.delete({
+          where: {
+            id: choices.id
+          }
+        });
+
+        break;
+      }
+    }
+
+    return reply.send(choices);
+  } catch (error) {
     return reply.status(500).send({
       error: 'Erro interno do servidor'
     });
